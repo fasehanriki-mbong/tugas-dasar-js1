85f654de9b7fdbae6a2c8d20cb350cdbe7bc7f3a
fasehanriki-mbong <fasehanriki@gmail.com>
2025-09-14 01:07:44 +0700

    edit tugas

--- nilai.js
+++ nilai.js
@@ -21,17 +21,17 @@ function tentukanGrade(hasilNilaiAkhir) {
 
 // Data 5 mahasiswa
 let mahasiswa = [
-  { nama: "Riki", absen: 22, tugas: 80, uts: 55, uas: 70 },
-  { nama: "Ani", absen: 5, tugas: 90, uts: 65, uas: 85 },
-  { nama: "Budi", absen: 11, tugas: 75, uts: 70, uas: 60 },
-  { nama: "Citra", absen: 14, tugas: 60, uts: 50, uas: 55 },
-  { nama: "Dewi", absen: 18, tugas: 95, uts: 85, uas: 90 },
+  { tugas: 80, uts: 55, uas: 70 },
+  { tugas: 90, uts: 65, uas: 85 },
+  { tugas: 75, uts: 70, uas: 60 },
+  { tugas: 60, uts: 50, uas: 55 },
+  { tugas: 95, uts: 85, uas: 90 },
 ];
 
 // Looping hitung nilai & grade
 for (let i = 0; i < mahasiswa.length; i++) {
-  let mhs = mahasiswa[i];
-  let nilaiAkhir = hitungNilaiAkhir(mhs.tugas, mhs.uts, mhs.uas);
+  let mahsiswa  = mahasiswa[i];
+  let nilaiAkhir = hitungNilaiAkhir(mahsiswa.tugas, mahsiswa.uts, mahsiswa.uas);
   let grade = tentukanGrade(nilaiAkhir);
 
   console.log(
